a0690a9068aca3305d56c2f5af29f1193a1fbd10
wxj <wxjcipher@163.com>
2021-10-07 16:11:13 +0800

    动态规划

--- untitled/src/com/company/longestPalindrome.java
+++ untitled/src/com/company/longestPalindrome.java
@@ -16,6 +16,7 @@ public class longestPalindrome {
     //暴力求解，（会报错，超出时间限制！！）
     public static String solution1(String s) {
         String res = String.valueOf(s.charAt(0)); //假设所有字符均不相同，不妨返回第一个元素
+
         for (int i = 0; i < s.length() - 1; i++) {
             for (int j = i + 1; j < s.length(); j++) { //遍历
                 if (s.charAt(i) == s.charAt(j)) { //找到首尾相同字符
@@ -39,10 +40,65 @@ public class longestPalindrome {
         return true;
     }
 
+    //官方解法，动态规划
+    public static String solution2(String s) {
+        int len = s.length();
+
+        //长度小于2的字符串返回其本身
+        if (len < 2) {
+            return s;
+        }
+
+        // dp[i][j] 表示 s[i..j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+
+        // 初始化：所有长度为 1 的子串都是回文串
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+
+        char[] charArray = s.toCharArray();
+
+        // 递推开始
+
+        int maxLen = 1;
+        int begin = 0;
+        // 先枚举子串长度
+        for (int L = 2; L <= len; L++) {
+            // 枚举左边界，左边界的上限设置可以宽松一些
+            for (int i = 0; i < len; i++) {
+                // 由 L 和 i 可以确定右边界，即 j - i + 1 = L
+                int j = i + L - 1;
+                // 如果右边界越界，就可以退出当前循环
+                if (j >= len) {
+                    break;
+                }
+
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false; //左边界和右边界不相等，返回false
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true; //L=2且相等，返回true
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1]; //L>2且左右边界相等，左边界右移，右边界左移
+                    }
+                }
+
+                // 只要 dp[i][i+L-1] == true 成立，就表示子串 s[i..i+L-1] 是回文，此时记录回文长度和起始位置
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("s= ");
         String s = sc.nextLine();
-        System.out.println(solution1(s));
+        //System.out.println(solution1(s));
+        System.out.println(solution2(s));
     }
 }
