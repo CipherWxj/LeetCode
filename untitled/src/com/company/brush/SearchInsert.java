de435b334e9c38aaad980dda2dc180d790458beb
wxj <wxjcipher@163.com>
2021-12-13 21:49:50 +0800

    查找插入位置

---
+++ untitled/src/com/company/brush/SearchInsert.java
@@ -0,0 +1,47 @@
+package com.company.brush;
+
+import java.util.Scanner;
+
+public class SearchInsert {
+
+    public static int solution(int[] nums, int target) {
+
+        int left = 0; // 初始化区间左索引
+        int right = nums.length - 1; // 初始化区间右索引
+
+        // 左索引小于右索引，遍历
+        while (left < right) {
+            int mid = (left + right) / 2; // 二分法取中间值
+            if (nums[mid + 1] < target) {
+                left = mid + 1; // 向上查找
+            } else if (nums[mid] > target) {
+                right = mid; // 向下查找
+            } else if (nums[mid] < target && target <= nums[mid + 1]) {
+                return mid + 1; // 判断在两数中间，返回插入位置
+            } else {
+                return mid; // 判断与中间数相等，返回该位置
+            }
+        }
+        // 如果比数组最大值还要大，返回最右侧插入位置，nums.length
+        if (target > nums[nums.length - 1]) {
+            return nums.length;
+        }
+        // 如果比数组最小值还要小，返回最左侧插入位置，0
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("请输入一个升序排列的数组：");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        s = s.substring(1, s.length() - 1);
+        String[] str = s.split(",");
+        int[] arr = new int[str.length];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        System.out.println("请输入所要查找的值：");
+        int target = sc.nextInt();
+        System.out.println(solution(arr, target));
+    }
+}
