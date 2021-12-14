eab5ca384ba5ec31cd77af3b19dad4f549bdbaa9
wxj <wxjcipher@163.com>
2021-12-14 11:28:02 +0800

    查找插入位置

--- untitled/src/com/company/brush/SearchInsert.java
+++ untitled/src/com/company/brush/SearchInsert.java
@@ -1,3 +1,13 @@
+/**
+ * @author: Wxj
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+ * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ * <p>输入描述:
+ * nums = [1,3,5,6], target = 5
+ * <p>输出描述:
+ * 2
+ */
 package com.company.brush;
 
 import java.util.Scanner;
