fbe747db90b3fc900bf617152ad1e7f6d11b44a1
Xinjian Wang <wxjcipher@163.com>
2022-02-08 17:36:08 +0800

    215. 数组中的第K个最大元素

--- untitled/src/com/company/brush/sort/heapsort/FindKthLargest.java
+++ untitled/src/com/company/brush/sort/heapsort/FindKthLargest.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 215. 数组中的第K个最大元素
  * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
  * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
  * <p>输入描述:
@@ -7,7 +8,7 @@
  * <p>输出描述:
  * 5
  */
-package com.company.brush;
+package com.company.brush.sort.heapsort;
 
 import java.util.Scanner;
 
