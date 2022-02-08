4d62616283a20a9554cb3201251713cfa99c38cd
Xinjian Wang <wxjcipher@163.com>
2022-02-08 13:58:34 +0800

    找到数组中重复元素

---
+++ untitled/src/com/company/brush/ContainsDuplicate.java
@@ -0,0 +1,38 @@
+/**
+ * @author: Wxj
+ * 给你一个整数数组 nums 。如果任一值在数组中出现 至少两次 ，返回 true ；如果数组中每个元素互不相同，返回 false 。
+ * <p>输入描述:
+ * nums = [1,2,3,1]
+ * <p>输出描述:
+ * true
+ */
+package com.company.brush;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public static boolean solution(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int x : nums) {
+            if (!set.add(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个数组：");
+        String s = sc.nextLine();
+        s = s.substring(1, s.length() - 1);
+        String[] str = s.split(",");
+        int[] nums = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            nums[i] = Integer.parseInt(str[i]);
+        }
+        System.out.println(solution(nums));
+    }
+}
