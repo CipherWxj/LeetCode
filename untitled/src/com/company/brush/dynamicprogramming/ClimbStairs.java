3a3d1c6b32ce1d624cd557c0ec799c5ba9fadbc7
Xinjian Wang <wxjcipher@163.com>
2022-02-08 17:17:20 +0800

    70. 爬楼梯

--- untitled/src/com/company/brush/dynamicprogramming/ClimbStairs.java
+++ untitled/src/com/company/brush/dynamicprogramming/ClimbStairs.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 70. 爬楼梯
  * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
  * 注意：给定 n 是一个正整数。
@@ -8,7 +9,7 @@
  * <p>输出描述:
  * 2
  */
-package com.company.brush;
+package com.company.brush.dynamicprogramming;
 
 import java.util.Scanner;
 
