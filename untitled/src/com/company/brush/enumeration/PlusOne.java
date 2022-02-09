d9bc21b75a1bec31ab27768a3b92b9db4a51f307
Xinjian Wang <wxjcipher@163.com>
2022-02-09 15:09:00 +0800

    66. 加一

--- untitled/src/com/company/brush/enumeration/PlusOne.java
+++ untitled/src/com/company/brush/enumeration/PlusOne.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 66. 加一
  * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
  * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
  * 你可以假设除了整数 0 之外，这个整数不会以零开头。
@@ -8,7 +9,7 @@
  * <p>输出描述:
  * [1,2,4]
  */
-package com.company.brush;
+package com.company.brush.enumeration;
 
 import java.util.Arrays;
 import java.util.Scanner;
