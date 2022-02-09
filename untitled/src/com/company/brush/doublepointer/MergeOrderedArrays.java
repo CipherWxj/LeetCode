2df73c1f489cecdf206dd449f349ee817a5f4d7f
Xinjian Wang <wxjcipher@163.com>
2022-02-09 14:21:35 +0800

    88. 合并两个有序数组

--- untitled/src/com/company/brush/doublepointer/MergeOrderedArrays.java
+++ untitled/src/com/company/brush/doublepointer/MergeOrderedArrays.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 88. 合并两个有序数组
  * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，
  * 另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
  * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
@@ -10,7 +11,7 @@
  * <p>输出描述:
  * [1,2,2,3,5,6]
  */
-package com.company.brush;
+package com.company.brush.doublepointer;
 
 import java.util.Arrays;
 import java.util.Scanner;
