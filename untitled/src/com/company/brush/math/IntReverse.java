c0c341f5e98a5a7cf45a489ea621d5871412c839
Xinjian Wang <wxjcipher@163.com>
2022-02-09 13:13:19 +0800

    7. 整数反转

--- untitled/src/com/company/brush/math/IntReverse.java
+++ untitled/src/com/company/brush/math/IntReverse.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 7. 整数反转
  * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
  * 如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
  * 假设环境不允许存储 64 位整数（有符号或无符号）。
@@ -8,7 +9,7 @@
  * <p>输出描述:
  * 321
  */
-package com.company.brush;
+package com.company.brush.math;
 
 import java.util.Scanner;
 
