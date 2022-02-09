7bbefd9ad00b73977208e2d182ca9ed899f9d14a
Xinjian Wang <wxjcipher@163.com>
2022-02-09 14:40:41 +0800

    9. 回文数

--- untitled/src/com/company/brush/enumeration/PalindromeInt.java
+++ untitled/src/com/company/brush/enumeration/PalindromeInt.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 9. 回文数
  * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
  * 回文数：正序（从左向右）和倒序（从右向左）读都是一样的整数。例如，121 是回文，而 123 不是。
  * <p>输入描述:
@@ -7,7 +8,7 @@
  * <p>输出描述:
  * true
  */
-package com.company.brush;
+package com.company.brush.enumeration;
 
 
 import java.util.Scanner;
