daab2fbcf387278d4dc1dd7227d73920900633e3
Xinjian Wang <wxjcipher@163.com>
2022-02-09 13:29:49 +0800

    169. 多数元素

--- untitled/src/com/company/brush/moorevote/MajorityElement2.java
+++ untitled/src/com/company/brush/moorevote/MajorityElement2.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 169. 多数元素
  * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于n/2 的元素。
  * 假设数组是非空的，并且给定的数组总是存在多数元素。
  * <p>输入描述:
@@ -7,7 +8,7 @@
  * <p>输出描述:
  * 2
  */
-package com.company.brush;
+package com.company.brush.moorevote;
 
 import java.util.Scanner;
 
