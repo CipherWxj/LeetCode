6daa880fc983250137253eec6523fcb089ede74d
Xinjian Wang <wxjcipher@163.com>
2022-02-09 13:16:31 +0800

    283. 移动零

--- untitled/src/com/company/brush/doublepointer/MoveZeroes.java
+++ untitled/src/com/company/brush/doublepointer/MoveZeroes.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 283. 移动零
  * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
  * 必须在原数组上操作，不能拷贝额外的数组。
  * 尽量减少操作次数。
@@ -8,7 +9,7 @@
  * <p>输出描述:
  * [1,3,12,0,0]
  */
-package com.company.brush;
+package com.company.brush.doublepointer;
 
 import java.util.Arrays;
 import java.util.Scanner;
