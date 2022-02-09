b28e3f4eb7dceb79671da6a9dedf2e2358c567c5
Xinjian Wang <wxjcipher@163.com>
2022-02-09 15:19:44 +0800

    35. 搜索插入位置

--- untitled/src/com/company/brush/search/binarysearch/SearchInsert.java
+++ untitled/src/com/company/brush/search/binarysearch/SearchInsert.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 35. 搜索插入位置
  * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
  * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
  * 请必须使用时间复杂度为 O(log n) 的算法。
@@ -8,7 +9,7 @@
  * <p>输出描述:
  * 2
  */
-package com.company.brush;
+package com.company.brush.search.binarysearch;
 
 import java.util.Scanner;
 
