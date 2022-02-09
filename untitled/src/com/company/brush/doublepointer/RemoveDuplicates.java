639b946f09c2509cc8b25b6282a2d58366415385
Xinjian Wang <wxjcipher@163.com>
2022-02-09 15:11:31 +0800

    26. 删除有序数组中的重复项

--- untitled/src/com/company/brush/doublepointer/RemoveDuplicates.java
+++ untitled/src/com/company/brush/doublepointer/RemoveDuplicates.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 26. 删除有序数组中的重复项
  * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
  * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
  * <p>输入描述:
@@ -7,7 +8,7 @@
  * <p>输出描述:
  * 2
  */
-package com.company.brush;
+package com.company.brush.doublepointer;
 
 import java.util.Scanner;
 
