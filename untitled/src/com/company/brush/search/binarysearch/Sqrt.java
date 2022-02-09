d1f09d54ad5d26a869e5e898d70be798bde36a62
Xinjian Wang <wxjcipher@163.com>
2022-02-09 15:25:08 +0800

    69. x的平方根

--- untitled/src/com/company/brush/search/binarysearch/Sqrt.java
+++ untitled/src/com/company/brush/search/binarysearch/Sqrt.java
@@ -1,5 +1,6 @@
 /**
  * @author: Wxj
+ * 69. x的平方根
  * 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
  * 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
  * 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
@@ -8,7 +9,7 @@
  * <p>输出描述:
  * 2
  */
-package com.company.brush;
+package com.company.brush.search.binarysearch;
 
 import java.util.Scanner;
 
