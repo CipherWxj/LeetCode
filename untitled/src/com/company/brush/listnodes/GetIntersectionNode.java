013f1bfd0ba1a29f728eb732de22dc751f9a1637
wxj <wxjcipher@163.com>
2022-03-09 17:21:09 +0800

    160. 相交链表

---
+++ untitled/src/com/company/brush/listnodes/GetIntersectionNode.java
@@ -0,0 +1,43 @@
+/**
+ * @author: Wxj
+ * 160. 相交链表
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。
+ * 如果两个链表不存在相交节点，返回 null 。
+ * 题目数据 保证 整个链式结构中不存在环。
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ * <p>输入描述:
+ * intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+ * <p>输出描述:
+ * Intersected at '8'
+ */
+package com.company.brush.listnodes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GetIntersectionNode {
+    // 哈希表
+    public ListNode solution1(ListNode headA, ListNode headB) {
+        Set<ListNode> set = new HashSet<>();
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (set.contains(headB)) return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+
+    // 双指针
+    public ListNode solution2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode a = headA, b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
