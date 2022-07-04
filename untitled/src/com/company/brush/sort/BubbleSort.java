d2cf6f0c6ee39b4c4659f14ad8cb1554e75fa889
Wang Xinjian <78528668+CipherWxj@users.noreply.github.com>
2022-07-04 14:46:38 +0800

    Update BubbleSort.java

--- untitled/src/com/company/brush/sort/BubbleSort.java
+++ untitled/src/com/company/brush/sort/BubbleSort.java
@@ -1,11 +1,12 @@
-// 比较相邻元素，根据大小调整先后位置
-// 从前往后，依次将最大（小）的元素放到最后
-// 时间复杂度 O(n^2)，空间复杂度 O(1)
-class BubbleSort{
+public class SortArray {
   public int[] solution(int[] nums){
     return bubbleSort(int[] nums);
   }
   
+  // 冒泡排序
+  // 比较相邻元素，根据大小调整先后位置
+  // 从前往后，依次将最大（小）的元素放到最后
+  // 时间复杂度 O(n^2)，空间复杂度 O(1)
   public int[] bubbleSort(int[] nums){
     // 进行 n-1 次，第一次找到最大（小）元素放到最后，第二次找到第二大（小）放到倒数第二的位置……
     for(int i = nums.length - 1; i > 0; i--){
