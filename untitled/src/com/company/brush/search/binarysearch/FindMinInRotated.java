e1658928f115b25b0f5b7ec1d19da5d8513f9914
Xinjian Wang <wxjcipher@163.com>
2022-07-07 22:34:27 +0800

    153. 寻找旋转排序数组中的最小值

---
+++ untitled/src/com/company/brush/search/binarysearch/FindMinInRotated.java
@@ -0,0 +1,37 @@
+/**
+ * @author: Wxj
+ * 153. 寻找旋转排序数组中的最小值
+ * 已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。
+ * 例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+ * 若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+ * 若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+ * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ * 给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。
+ * 请你找出并返回数组中的 最小元素 。
+ * 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+ * <p>输入描述:
+ * nums = [4,5,6,7,0,1,2]
+ * <p>输出描述:
+ * 0
+ */
+package com.company.brush.search.binarysearch;
+
+public class FindMinInRotated {
+    // 旋转排序数组的性质：若无重复元素，旋转之后一定有 最左侧元素 大于 最右侧元素
+    public int solution(int[] nums){
+        int left = 0, right = nums.length - 1;
+        // 没有旋转的情况，一定是最左侧最小
+        if(nums[left] < nums[right]) return nums[left];
+        // 二分查找
+        while (left < right){
+            int mid = left + (right - left) / 2;
+            // 如果中间元素 小于 最右侧元素，说明 mid 右侧一直是递增的，最小值在 mid 左侧（含mid）
+            if(nums[mid] < nums[right]){
+                right = mid;
+            }else { // 如果中间元素 大于 最右侧元素，说明 mid 左侧一直是递增的，最小值在 mid 左侧（不含mid）
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+}
