803d36fc917823ee783c3eb53827a74c86d884bc
Xinjian Wang <wxjcipher@163.com>
2022-08-01 23:50:27 +0800

    53. 最大子数组和

--- untitled/src/com/company/brush/dynamicprogramming/MaxSubArraySum.java
+++ untitled/src/com/company/brush/dynamicprogramming/MaxSubArraySum.java
@@ -7,6 +7,7 @@
  * [-2,1,-3,4,-1,2,1,-5,4]
  * <p>输出描述:
  * 6
+ * 注：下面代码稍微复杂，为了找出符合条件的一个子数组。
  */
 package com.company.brush.dynamicprogramming;
 
@@ -16,12 +17,30 @@ public class MaxSubArraySum {
 
     // 动态规划
     public static int solution(int[] nums) {
-        int[] dp = new int[nums.length]; // dp[] 记录以i为结尾的子数组的和
-        int max = nums[0]; // 最大值存储
-        dp[0] = nums[0]; // 初始状态
+        int pre = nums[0]; // 记录以 i位置 为结尾的子数组的最大和
+        int max = nums[0]; // 最大值
+        int l = 0, r = 0; // 每一位 子数组的左右位置
+        int ml = 0, mr = 0; // 保存最大和子数组的左右位置
         for (int i = 1; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]); // 只需要比较 前面子串与该位置的和 与 该位置的值 的大小
-            max = Math.max(max, dp[i]); // 与存储的最大值进行比较
+            // 前面最大和子数组 + 该位置 > 该位置
+            // 子数组起始位置不变
+            if (pre + nums[i] > nums[i]) {
+                pre += nums[i];
+            } else {
+                // 前面最大和子数组 + 该位置 <= 该位置
+                // 子数组起始位置变为当前位置
+                pre = nums[i];
+                l = i;
+            }
+            // 子数组终止位置变为当前位置
+            r = i;
+
+            // 更新最大值
+            if(max < pre) {
+                max = pre;
+                ml = l;
+                mr = r;
+            }
         }
         return max;
     }
