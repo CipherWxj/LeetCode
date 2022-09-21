46f11dc3882d5412db946f019e7fc8be8c8b8117
Xinjian Wang <wxjcipher@163.com>
2022-09-21 11:06:05 +0800

    9.20 b站

--- untitled/src/com/company/bishi/bilibili/Main2.java
+++ untitled/src/com/company/bishi/bilibili/Main2.java
@@ -3,7 +3,7 @@ package com.company.bishi.bilibili;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
