e70db48a027daf89dfa742ef2af92bcb9d1e1529
wangxinjian <wxjcipher@163.com>
2024-07-05 22:54:06 +0800

    76. 最小覆盖子串

--- src/main/java/com.company/brush/slidingwindow/MinWindow.java
+++ src/main/java/com.company/brush/slidingwindow/MinWindow.java
@@ -14,53 +14,49 @@ package com.company.brush.slidingwindow;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 public class MinWindow {
     public static String solution(String s, String t) {
-        if (s.length() < t.length() || t.length() == 0) return "";
-        // 分别记录字符串s、t中的字符个数
-        Map<Character, Integer> mapT = new HashMap<>();
-        Map<Character, Integer> mapS = new HashMap<>();
-        // 将 t 中的所有字符标记记录
+        if (s.length() < t.length()) return "";
+
+        Map<Character, Integer> tMap = new HashMap<>();
+
         for (int i = 0; i < t.length(); i++) {
-            mapT.put(t.charAt(i), mapT.getOrDefault(t.charAt(i), 0) + 1);
+            char c = t.charAt(i);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
-        // 初始化包含 t 中所有元素的子串
-        String res = "";
-        // count 表示 s.substring(start, end + 1)中包含 t 中字符的个数
-        // resLen 表示 包含t中所有字符的子串的最小长度
-        int count = 0, resLen = Integer.MAX_VALUE;
-        // 滑动窗口的起止位置
-        int start = 0, end;
-        for (end = 0; end < s.length(); end++) {
-            // 记录 s 中的字符
-            mapS.put(s.charAt(end), mapS.getOrDefault(s.charAt(end), 0) + 1);
-            // 有效字符
-            // t 中存在，且已经遍历的 s 中该字符的个数 比 t 中 少
-            if (mapT.containsKey(s.charAt(end)) && mapS.get(s.charAt(end)) <= mapT.get(s.charAt(end))) {
-                count++;
+
+        Map<Character, Integer> sMap = new HashMap<>();
+
+        int left = 0, right = 0;
+        int valid = 0, start = 0, minLen = Integer.MAX_VALUE;
+
+        while (right < s.length()) {
+            char cur = s.charAt(right);
+            right++;
+            if (tMap.containsKey(cur)) {
+                sMap.put(cur, sMap.getOrDefault(cur, 0) + 1);
+                if (sMap.get(cur).equals(tMap.get(cur))) valid++;
             }
-            // 更新窗口的起始位置
-            // t 中不含起始位置字符（保证start<=end），或者 起始位置字符多余
-            while (start < end && (!mapT.containsKey(s.charAt(start)) || mapS.get(s.charAt(start)) > mapT.get(s.charAt(start)))) {
-                mapS.put(s.charAt(start), mapS.get(s.charAt(start)) - 1);
-                start++;
-            }
-            // 找到包含 t中所有字符的子串，记录该子串及其长度
-            // 后续一直遍历到s结尾，如果符合条件的子串长度更小，则更新
-            if (count == t.length() && end - start + 1 < resLen) {
-                resLen = end - start + 1;
-                res = s.substring(start, end + 1);
+
+            while (valid == tMap.size()) {
+
+                if (right - left < minLen) {
+                    start = left;
+                    minLen = right - left;
+                }
+                char leftChar = s.charAt(left);
+                left++;
+                if (tMap.containsKey(leftChar)) {
+                    if (sMap.get(leftChar).equals(tMap.get(leftChar))) valid--;
+                    sMap.put(leftChar, sMap.getOrDefault(leftChar, 0) - 1);
+                }
             }
         }
-        return res;
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        String t = sc.nextLine();
-        System.out.println(solution(s, t));
+        System.out.println(solution("ADOBECODEBANC", "ABC"));
     }
 }
