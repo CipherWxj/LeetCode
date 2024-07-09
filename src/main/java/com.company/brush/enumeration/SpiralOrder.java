25811688f4b273948c16870c3aacee3d7b74c5a3
wangxinjian <wxjcipher@163.com>
2024-07-09 23:53:42 +0800

    54. 螺旋矩阵

--- src/main/java/com.company/brush/enumeration/SpiralOrder.java
+++ src/main/java/com.company/brush/enumeration/SpiralOrder.java
@@ -9,43 +9,49 @@
  */
 package com.company.brush.enumeration;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SpiralOrder {
     public static List<Integer> solution(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return null;
-
-        List<Integer> list = new ArrayList<>();
-        int numRow = matrix.length, numColumn = matrix[0].length;
-        int left = 0, right = numColumn - 1, top = 0, bottom = numRow - 1;
-
+        List<Integer> res = new ArrayList<>();
+        if (matrix.length == 0 || matrix[0].length == 0) return res;
+        // 初始化最外圈上下左右四条线
+        int top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
+        // 逐渐向内圈缩小
         while (left <= right && top <= bottom) {
-            // 上边界
-            for (int j = left; j <= right; j++) {
-                list.add(matrix[top][j]);
+            // 顺时针
+            // 先遍历最上边一行，从左上顶点开始遍历，右上顶点放到下面最右列遍历
+            for (int r = left; r < right; r++) {
+                res.add(matrix[top][r]);
             }
-            // 右边界
-            for (int i = top + 1; i <= bottom; i++) {
-                list.add(matrix[i][right]);
+            // 再遍历最右边一列，从右上顶点开始遍历，因为有下面的if，右下顶点也要遍历到，避免只剩一行或一列时少遍历
+            for (int b = top; b <= bottom; b++) {
+                res.add(matrix[b][right]);
             }
-            // 这里的判断很重要！处理最后的中间元素
-            // 想象成 如果最后还是个 方框，就去搜 四边； 如果不是 方框， 就只搜索 两边（其实只执行了一边）
+            // 为了兼容最内圈只剩一行或者一列的情况，防止重复遍历
             if (left < right && top < bottom) {
-                // 下边界
-                for (int j = right - 1; j >= left; j--) {
-                    list.add(matrix[bottom][j]);
+                // 再遍历最下边一行，从右下顶点左侧元素开始遍历，左下顶点放到最左列遍历
+                for (int l = right - 1; l > left; l--) {
+                    res.add(matrix[bottom][l]);
                 }
-                // 左边界
-                for (int i = bottom - 1; i > top; i--) {
-                    list.add(matrix[i][left]);
+                // 最后遍历最左边一列，从左下顶点开始遍历，左上顶点已经遍历过了，不用遍历
+                for (int t = bottom; t > top; t--) {
+                    res.add(matrix[t][left]);
                 }
             }
-            left++;
-            right--;
             top++;
+            right--;
             bottom--;
+            left++;
         }
-        return list;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(JSON.toJSONString(solution(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})));
     }
 }
