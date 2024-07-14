cbf53fdfddc8c3355bf48342b570a7a929ca95bc
wangxinjian <wxjcipher@163.com>
2024-07-14 23:55:05 +0800

    240. 搜索二维矩阵 II

--- src/main/java/com.company/brush/array_string/SearchMatrix.java
+++ src/main/java/com.company/brush/array_string/SearchMatrix.java
@@ -12,45 +12,54 @@
 package com.company.brush.array_string;
 
 public class SearchMatrix {
-    public static boolean solution(int[][] matrix, int target){
-        int m = matrix.length, n = matrix[0].length;
-
-//        // 暴力遍历
-//        for (int[] ints : matrix) {
-//            for (int j = 0; j < n; j++) {
-//                if (ints[j] == target) return true;
-//            }
-//        }
-//        return false;
-//
-//        // 对行二分
-//        for(int[] ints : matrix){
-//            int l = 0, r = n - 1;
-//            while (l < r){
-//                int mid = l + (r - l) / 2;
-//                if(ints[mid] == target) return true;
-//                if (ints[r] < target) break;
-//                if (ints[mid] < target){
-//                    l = mid + 1;
-//                }else {
-//                    r = mid;
-//                }
-//            }
-//        }
-//        return false;
-
-        if(n == 0) return false;
-        // 右上角开始
-        int i = 0, j = n - 1;
-        while (i < m && j >= 0){
-            if(matrix[i][j] < target){
-                i++; // 一行最右边值都比 target 小，那么这一行都比target 小，排除（大于行最大值）
-            }else if (matrix[i][j] > target){
-                j--; // 一列最上边值都比 target 大，那么这一行都比target 大，排除（小于列最小值）
-            }else {
-                return true; // 相等，找到了
+    // 暴力遍历
+    public static boolean solution(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                if (num == target) return true;
             }
         }
-        return false; // 没找到
+        return false;
+    }
+
+    // 二分查找
+    public static boolean solution1(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            int left = 0, right = row.length - 1;
+            // 逼近最后一个值，取等号
+            while (left <= right) {
+                int mid = (right - left) / 2 + left;
+                if (row[mid] == target) {
+                    return true;
+                } else if (row[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    // z字型遍历
+    public static boolean solution2(int[][] matrix, int target) {
+        int n = matrix.length, m = matrix[0].length;
+        // 从右上角开始！这是最巧妙的！
+        // 右上角的值，在某一行是最大的，在某一列是最小的
+        int i = 0, j = m - 1;
+        while (i < n && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) { // 所有位于第 i 行的元素都是严格小于 target，往更大的找，i++
+                i++;
+            } else { // 所有位于第 j 列的元素都是严格大于 target，往更小的找，j--
+                j--;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution2(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}}, 5));
     }
 }
