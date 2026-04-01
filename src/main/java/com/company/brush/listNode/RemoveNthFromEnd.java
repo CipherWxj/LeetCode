d117d959e5cd2a5613612b77470b8b9ed5c2105a
wangxinjian <wxjcipher@163.com>
2026-04-01 21:32:14 +0800

    feat: 删除链表的倒数第N个结点

--- src/main/java/com/company/brush/listNode/RemoveNthFromEnd.java
+++ src/main/java/com/company/brush/listNode/RemoveNthFromEnd.java
@@ -1,5 +1,7 @@
+package com.company.brush.listNode;
+
 /**
- * @author: Wxj
+ * @author: wangxinjian
  * 19. 删除链表的倒数第 N 个结点
  * 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
  * 假设链表节点总数大于等于 n。
@@ -8,32 +10,37 @@
  * <p>输出描述:
  * [1,2,3,5]
  */
-package com.company.brush.listNode;
-
 public class RemoveNthFromEnd {
-    public ListNode solution(ListNode head, int n) {
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        // 虚拟头节点
         ListNode dummy = new ListNode();
         dummy.next = head;
-        // 快慢指针，用于找到待删除节点的位置，这才是本题的重点
+        // 快指针先跑n步
         ListNode fast = dummy;
-        ListNode slow = dummy;
-
-        // 先让 fast 跑 n步
-        // 则剩余节点数为 Len - n
         for (int i = 0; i < n; i++) {
             fast = fast.next;
         }
-
-        // 再同时走，直到 fast 到最末尾节点
-        // 此时 slow 也走了 Len - n 步，slow.next 即为倒数第 n 个节点
+        // 慢指针从头开始，快慢指针同时走，二者的距离为n，直到快指针到达链表末尾
+        ListNode slow = dummy;
         while (fast.next != null) {
             fast = fast.next;
             slow = slow.next;
         }
-
-        // 删除 slow.next
-        slow.next = slow.next.next;
-        // 这里返回不返回 head，是因为 head 也可能被删除
+        // 要删除的节点就是 slow.next
+        ListNode needRemove = slow.next;
+        // 删除操作
+        slow.next = needRemove.next;
+        needRemove.next = null;
         return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        int n = 2;
+        ListNode result = removeNthFromEnd(head, n);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
     }
 }
