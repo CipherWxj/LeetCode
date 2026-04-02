3357bf0ef6cf7503ff04e59157a917d5aa8409be
wangxinjian <wxjcipher@163.com>
2026-04-02 14:32:25 +0800

    feat: K个一组翻转链表

--- src/main/java/com/company/brush/listNode/ReverseKGroup.java
+++ src/main/java/com/company/brush/listNode/ReverseKGroup.java
@@ -1,5 +1,7 @@
+package com.company.brush.listNode;
+
 /**
- * @author: Wxj
+ * @author: wangxinjian
  * 25. K 个一组翻转链表
  * 给你一个链表，每k个节点一组进行翻转，请你返回翻转后的链表。
  * k是一个正整数，它的值小于或等于链表的长度。
@@ -12,71 +14,55 @@
  * <p>输出描述:
  * [2,1,4,3,5]
  */
-package com.company.brush.listNode;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class ReverseKGroup {
-    public static ListNode solution(ListNode head, int k) {
-        ListNode dummy = new ListNode(); // 虚拟头节点，便于操作
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode();
+        // 虚拟头节点
         dummy.next = head;
-
-        ListNode prev = dummy; // 分段头节点的前一节点
+        // 前驱节点
+        ListNode prev = dummy;
+        // 用head做遍历节点
         while (head != null) {
-            ListNode tail = head; // 分段尾节点
-            // 分段操作
+            // 按k个节点分组，head是每组的第一个节点，tail是每组的最后一个节点
+            ListNode tail = head;
             for (int i = 1; i < k; i++) {
+                // 如果剩余节点不足k个，则不进行翻转
                 if (tail.next == null) return dummy.next;
                 tail = tail.next;
             }
-
-            ListNode next = tail.next; // 分段尾节点的下一节点
-            tail.next = null; // 断开，只取要反转的一段
-            prev.next = reverse(head); // 反转，并连接到前面已经完成反转的链表
-            head.next = next; // 连接到后面还未完成反转的链表，因为已经反转了，head来到了该段的最后
-            prev = head; // 更新prev
-            head = next; // 更新head，下一段开始
+            // 断开tail，便于反转操作，记录下一组的头节点
+            ListNode nextHead = tail.next;
+            tail.next = null;
+            // 反转，并连接反转后的链表
+            prev.next = reverse(head);
+            // 反转后的链表连接下一组待反转的节点
+            head.next = nextHead;
+            // 右移
+            prev = head;
+            head = nextHead;
         }
         return dummy.next;
     }
 
-    // 反转操作
     public static ListNode reverse(ListNode head) {
-        ListNode last = null;
+        ListNode pre = null;
         ListNode cur = head;
         while (cur != null) {
             ListNode next = cur.next;
-            cur.next = last;
-            last = cur;
+            cur.next = pre;
+            pre = cur;
             cur = next;
         }
-        return last;
+        return pre;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("请以数组形式输入一个链表：");
-        System.out.print("head= ");
-        String s = sc.nextLine();
-        s = s.substring(1, s.length() - 1);
-        String[] arr = s.split(",");
-        ListNode head = new ListNode(Integer.parseInt(arr[0]));
-        ListNode cur = head;
-        for (int i = 1; i < arr.length; i++) {
-            cur.next = new ListNode(Integer.parseInt(arr[i]));
-            cur = cur.next;
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        int k = 2;
+        ListNode result = reverseKGroup(head, k);
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
         }
-        System.out.println("请输入一个整数k：");
-        System.out.print("k= ");
-        int k = sc.nextInt();
-        ListNode res = solution(head, k);
-        List<Integer> r = new ArrayList<>();
-        while (res != null) {
-            r.add(res.val);
-            res = res.next;
-        }
-        System.out.println(r);
     }
 }
