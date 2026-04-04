f15887b2dd92f125dc8a91d958b92be99acacccc
wangxinjian <wxjcipher@163.com>
2026-04-04 23:55:52 +0800

    feat: 字符串解码

--- src/main/java/com/company/brush/stack/DecodeString.java
+++ src/main/java/com/company/brush/stack/DecodeString.java
@@ -1,5 +1,11 @@
+package com.company.brush.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Objects;
+
 /**
- * @author: Wxj
+ * @author: wangxinjian
  * 394. 字符串解码
  * 给定一个经过编码的字符串，返回它解码后的字符串。
  * 编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
@@ -10,50 +16,40 @@
  * <p>输出描述:
  * aaabcbc
  */
-package com.company.brush.stack;
-
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class DecodeString {
-    public static String solution(String s) {
-        // 维护两个栈，一个存字符，一个存数字
+    public static String decodeString(String s) {
+        // 一个栈保存数字，一个栈保存字符串
         Deque<String> strStack = new LinkedList<>();
         Deque<Integer> numStack = new LinkedList<>();
-
-        int count = 0; // 初始化计数器
-        StringBuilder res = new StringBuilder(); // 初始化结果字符串
-
+        // 保存遍历的字符串结果
+        StringBuilder res = new StringBuilder();
+        // 记录当前字符串需要循环的次数
+        int num = 0;
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-
-            // 数字，下一位还是数字的话就有多位数，计算
-            if (c >= '0' && c <= '9') {
-                count = count * 10 + c - '0';
-            } else if (c == '[') { // 左括号
-                // 将数字入栈，并将计数器置 0
-                numStack.push(count);
-                count = 0;
-                // 将已经确定的结果字符入栈，将结果串置为 “”
+            // 如果当前字符是数字，暂时保存，直到遇到下一个左括号，才是左括号里的字符需要循环的次数
+            if ('0' <= c && c <= '9') {
+                num = num * 10 + c - '0';
+            } else if ('a' <= c && c <= 'z') { // 如果当前字符是字母，暂时保存在结果里，直到遇到下一个左括号，才入栈暂存
+                res.append(c);
+            } else if (c == '[') { // 遇到左括号，将当前数字入栈，并重置数字，将当前字符串入栈，并重置字符串
+                numStack.push(num);
+                num = 0;
                 strStack.push(res.toString());
                 res = new StringBuilder();
-            } else if (c == ']') { // 右括号
-                // 将暂存的 结果串 循环解码
+            } else if (c == ']') { // 遇到右括号，将当前字符串循环k次，与栈顶字符串连接，入栈
+                int k = numStack.pop();
                 StringBuilder temp = new StringBuilder();
-                int n = numStack.removeLast();
-                for (int j = 0; j < n; j++) {
+                for (int j = 0; j < k; j++) {
                     temp.append(res);
                 }
-                // 与 前面已经确定的串 连接
-                res = new StringBuilder(strStack.removeLast() + temp);
-            } else { // 是字符（两个中括号之间的）放入结果串 暂存
-                res.append(c);
+                res = new StringBuilder(strStack.pop() + temp);
             }
         }
         return res.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(solution("3[a]2[bc]"));
+        System.out.println(decodeString("30[a]2[bc]"));
     }
 }
