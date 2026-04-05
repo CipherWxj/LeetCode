1fde15cb5e966e5adc5ca3f49305f4537e5063ec
wangxinjian <wxjcipher@163.com>
2026-04-05 23:02:27 +0800

    feat: 每日温度

--- src/main/java/com/company/brush/stack/DailyTemperatures.java
+++ src/main/java/com/company/brush/stack/DailyTemperatures.java
@@ -1,5 +1,11 @@
+package com.company.brush.stack;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
- * @author: Wxj
+ * @author: wangxinjian
  * 739. 每日温度
  * 给定一个整数数组 temperatures，表示每天的温度，返回一个数组 answer，
  * 其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
@@ -8,30 +14,29 @@
  * <p>输出描述:
  * [1,1,4,2,1,1,0,0]
  */
-package com.company.brush.stack;
-
-import java.util.Deque;
-import java.util.LinkedList;
-
 public class DailyTemperatures {
-    public int[] solution(int[] temperatures) {
+    public static int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
-        //if(n == 1) return new int[]{0}; // 这里可以不用考虑，java数组初始化默认元素全为 0
         int[] answer = new int[n];
-        // 维护一个单调栈，存储 temperatures 的下标
-        // 使得 栈底到栈顶 的 下标对应的元素 递减
-        Deque<Integer> stack = new LinkedList<>();
+        // 初始化单调递减栈，栈里存数组位置索引，栈内元素对应的温度栈底到栈顶递减
+        Deque<Integer> decStack = new LinkedList<>();
         for (int i = 0; i < n; i++) {
-            // 不为空 且 当前元素对应的温度 大于 栈顶元素对应的温度
-            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                // 出栈
-                int j = stack.poll();
-                // 计算栈顶元素位置的结果
+            // 如果栈不为空且当前温度大于栈顶索引对应的温度，说明找到了栈顶索引的下一个更高温度，直到栈为空或当前温度小于等于栈顶索引对应的温度
+            while (!decStack.isEmpty() && temperatures[decStack.peek()] < temperatures[i]) {
+                // 栈顶索引出栈
+                int j = decStack.pop();
+                // 计算栈顶索引下一个更高温度出现在几天后
                 answer[j] = i - j;
             }
-            // 为空 或 当前元素对应的温度 小于 栈顶元素对应的温度，入栈
-            stack.push(i);
+            // 当前索引入栈
+            decStack.push(i);
         }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(Arrays.toString(dailyTemperatures(temperatures)));
+
     }
 }
