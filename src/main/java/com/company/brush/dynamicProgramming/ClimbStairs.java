d2d5a0049b68176460140d32116f0f12e7d20e90
wangxinjian <wxjcipher@163.com>
2026-04-07 17:27:27 +0800

    feat: 爬楼梯

--- src/main/java/com/company/brush/dynamicProgramming/ClimbStairs.java
+++ src/main/java/com/company/brush/dynamicProgramming/ClimbStairs.java
@@ -1,5 +1,7 @@
+package com.company.brush.dynamicProgramming;
+
 /**
- * @author: Wxj
+ * @author: wangxinjian
  * 70. 爬楼梯
  * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
@@ -9,36 +11,38 @@
  * <p>输出描述:
  * 2
  */
-package com.company.brush.dynamicProgramming;
-
-import java.util.Scanner;
-
 public class ClimbStairs {
     // 动态规划
-    public static int solution(int n) {
-        if (n == 1) return 1; // 1级台阶，方案数：1
-        if (n == 2) return 2; // 2级台阶，方案数：2
-
-        // 初始化数组记录方案数，dp[0]空出，不做操作
-        int[] dp = new int[n + 1];
-
-        // 初始化边界条件
-        dp[1] = 1; // 1级台阶，方案数：1
-        dp[2] = 2; // 2级台阶，方案数：2
-
-        // 递推
-        for (int i = 3; i <= n; i++) {
-            // 每次能走的台阶数只能是         走1级或者                    走2级
-            // 因此到达第i级台阶的方案数dp[i]=到达第i-1级台阶的方案数dp[i-1]+到达第i-2级台阶的方案数dp[i-2]
+    public static int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        // 初始化动态数组，dp[i]表示到达第i+1级台阶的方法数
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        // 要到达第n级台阶，要么从n-1级台阶走1步，要么从n-2级台阶走2步
+        // 所以到达第i+1级台阶的方法数 等于 到达第i级台阶的方法数 加 到达第i-1级台阶的方法数
+        for (int i = 2; i < n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return dp[n];
+        return dp[n - 1];
+    }
+
+    public static int climbStairsPLus(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int dp1 = 1;
+        int dp2 = 2;
+        for (int i = 2; i < n; i++) {
+            int dp3 = dp1 + dp2;
+            dp1 = dp2;
+            dp2 = dp3;
+        }
+        return dp2;
     }
 
     public static void main(String[] args) {
-        System.out.println("请输入一个正整数：");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(solution(n));
+        int n = 2;
+        System.out.println(climbStairs(n));
     }
 }
