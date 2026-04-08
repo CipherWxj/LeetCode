405a1d1369c5c722f7d2df04541488141a63384b
wangxinjian <wxjcipher@163.com>
2026-04-08 16:45:51 +0800

    feat: 打家劫舍

--- src/main/java/com/company/brush/dynamicProgramming/Rob.java
+++ src/main/java/com/company/brush/dynamicProgramming/Rob.java
@@ -1,5 +1,7 @@
+package com.company.brush.dynamicProgramming;
+
 /**
- * @author: Wxj
+ * @author: wangxinjian
  * 198. 打家劫舍
  * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，
  * 影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
@@ -11,23 +13,23 @@
  * 解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
  * 偷窃到的最高金额 = 2 + 9 + 1 = 12 。
  */
-package com.company.brush.dynamicProgramming;
-
 public class Rob {
-    public int solution(int[] nums) {
-        int n = nums.length;
-        // 动态数组
-        // dp[i + 1]表示到达第 i 间房子时的最大金额
-        // 前两位dp[0]、dp[1]为了递推赋初值0
-        int[] dp = new int[n + 2];
-//       // 初始值
-//        dp[0] = 0;
-//        dp[1] = 0;
-        dp[2] = nums[0];
-        // 递推
-        for (int i = 3; i < n + 2; i++) {
-            dp[i] = Math.max(dp[i - 2] + nums[i - 2], dp[i - 1]);
+    public static int rob(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        // 初始化动态变量，lastTwo表示偷窃到上上间房屋的最大金额，lastOne表示偷窃到上间房屋的最大金额
+        // 到第i间房屋时，小偷只有两种选择，要么偷窃：上上间房屋的最大金额 + 当前房屋金额，要么不偷窃：上间房屋的最大金额
+        int lastTwo = nums[0];
+        int lastOne = Math.max(lastTwo, nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int cur = Math.max(lastTwo + nums[i], lastOne);
+            lastTwo = lastOne;
+            lastOne = cur;
         }
-        return dp[n + 1];
+        return lastOne;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 9, 3, 1};
+        System.out.println(rob(nums));
     }
 }
