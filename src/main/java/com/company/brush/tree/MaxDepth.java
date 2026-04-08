084b77bdf9a2e1caa71f4263f6182dcdb5b12230
wangxinjian <wxjcipher@163.com>
2026-04-08 20:49:33 +0800

    feat: 二叉树的最大深度

--- src/main/java/com/company/brush/tree/MaxDepth.java
+++ src/main/java/com/company/brush/tree/MaxDepth.java
@@ -1,5 +1,10 @@
+package com.company.brush.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- * @author: Wxj
+ * @author: wangxinjian
  * 104. 二叉树的最大深度
  * 给定一个二叉树，找出其最大深度。
  * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
@@ -9,35 +14,25 @@
  * <p>输出描述:
  * 3
  */
-package com.company.brush.tree.dfs;
-
-import com.company.brush.tree.TreeNode;
-
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class MaxDepth {
-
-    // DFS
-    public int dfs(TreeNode root, int max) {
+    // 深度优先搜索
+    public static int maxDepthDFS(TreeNode root) {
         // 终止条件
-        if (root == null) return max;
-        // 左子树最大深度
-        int leftDepth = dfs(root.left, max);
-        // 右子树最大深度
-        int rightDepth = dfs(root.right, max);
-        // 取最大，并加上父节点的深度
-        max = Math.max(leftDepth, rightDepth) + 1;
-        return max;
+        if (root == null) return 0;
+        // 递归左子树和右子树，取最大值加1
+        int leftDepth = maxDepthDFS(root.left);
+        int rightDepth = maxDepthDFS(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
     }
 
-    // BFS
-    public int bfs(TreeNode root) {
+    // 广度优先搜索
+    public static int maxDepthBFS(TreeNode root) {
         if (root == null) return 0;
         int max = 0;
+        // 初始化队列
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        // 层序遍历，每遍历完一层深度 +1
+        // 层序遍历，每遍历完一层深度加1
         while (!queue.isEmpty()) {
             int n = queue.size();
             for (int i = 0; i < n; i++) {
