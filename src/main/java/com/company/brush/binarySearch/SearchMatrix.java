d1bde09ec90628627080d2983087ab0306e9ac0d
wangxinjian <wxjcipher@163.com>
2026-04-09 21:08:43 +0800

    feat: 搜索二维矩阵

--- src/main/java/com/company/brush/binarySearch/SearchMatrix.java
+++ src/main/java/com/company/brush/binarySearch/SearchMatrix.java
@@ -1,5 +1,5 @@
 /**
- * @author: Wxj
+ * @author: wangxinjian
  * 74. 搜索二维矩阵
  * 编写一个高效的算法来判断 m x n 矩阵中，是否存在一个目标值。该矩阵具有如下特性：
  * 每行中的整数从左到右按升序排列。
@@ -14,42 +14,45 @@
 package com.company.brush.binarySearch;
 
 public class SearchMatrix {
-    public boolean solution(int[][] matrix, int target) {
-        // 找到目标值可能存在的 行
-        int row = binSearchRow(matrix, target);
-        // 左边界不存在
-        if (row < 0) return false;
-        // 在可能存在的行 找目标值
-        return binSearchTarget(matrix[row], target);
-    }
-
-    public int binSearchRow(int[][] matrix, int target) {
-        // 左开右闭区间
-        int left = -1, right = matrix.length - 1;
-        while (left < right) {
-            int mid = left + (right + 1 - left) / 2; // +1 因为初始 left = -1
-            if (matrix[mid][0] <= target) {
-                left = mid;
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int row = searchRow(matrix, target);
+        // 如果行不存在，返回 false
+        if (row == -1) return false;
+        // 二分查找对应的行
+        int left = 0, right = matrix[row].length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] < target) {
+                left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return left;
+        return false;
     }
 
-    // 左闭右开区间上的二分法
-    public boolean binSearchTarget(int[] nums, int target) {
-        int left = 0, right = nums.length;
-        while (left < right) {
+    // 二分查找行
+    public static int searchRow(int[][] matrix, int target) {
+        int left = 0, right = matrix.length - 1;
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                return true;
-            } else if (nums[mid] < target) {
+            if (matrix[mid][0] <= target) {
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid - 1;
             }
         }
-        return false;
+        // 最后一次比较 mid = left = right，
+        // 如果 matrix[mid][0] <= target，说明 target 可能在 matrix[mid] 中，返回 right；
+        // 否则，说明 target 可能在 matrix[mid - 1] 中，执行right - 1，返回 right
+        return right;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+        int target = 16;
+        System.out.println(searchMatrix(matrix, target));
     }
 }
